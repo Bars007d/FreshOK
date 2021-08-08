7775f4eab91aa28adda8a796f6cb13aea8199bc1
AStin <aragorn005d@mail.ru>
2021-08-09 01:13:54 +0700

    add footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-	
+	$('.header__filter-btn').on('click', function(){
+		$('.header__filter-list').toggleClass('header__filter-list--active');
+		$('.header__filter-btn').toggleClass('header__filter-btn--active');
+	});
 })
